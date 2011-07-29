94ef35133f3e3c7192365574682621110670a6e8
Davide Angelocola <davide.angelocola@emaze.net>
2011-07-29 13:57:17 +0200

    fix: syntax error

--- src/capture.c
+++ src/capture.c
@@ -34,7 +34,7 @@
 int capture(struct packet *packet, int fd, int loindex)
 {
     struct sockaddr_ll from;
-    socklen_t fromlen = sizeof(struct sockaddr_ll)
+    socklen_t fromlen = sizeof(struct sockaddr_ll);
     memset(packet, 0, sizeof(struct packet));
 
     if (recvfrom(fd, packet->base, PKT_DATA_LEN, MSG_TRUNC,
