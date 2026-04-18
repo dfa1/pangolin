5bee9a49a93ab4b72efb106e10ca70475692c904
Davide Angelocola <davide.angelocola@gmail.com>
2026-04-18 15:15:42 +0200

    fix: compile time issue in amd64

--- src/if.c
+++ src/if.c
@@ -182,7 +182,7 @@ int if_open(const char *iface)
     struct sockaddr_ll sll;
     int fd;
     int err;
-    size_t errlen = sizeof(err);
+    socklen_t errlen = sizeof(err);
 
     fd = socket(PF_PACKET, SOCK_RAW, TONET16(ETH_P_ALL));	// TODO: extension point
 
